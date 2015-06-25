c047779d69f51d9a3f462f0015d6584cfce18e82
darui00kara <Takeshi00Miyamoto@gmail.com>
2015-06-26 00:00:53 +0900

    Finish filling_in_layout

--- sample_app/web/router.ex
+++ sample_app/web/router.ex
@@ -16,9 +16,11 @@ defmodule SampleApp.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/signup", UserController, :new
     get "/home", StaticPagesController, :home
     get "/help", StaticPagesController, :help
     get "/about", StaticPagesController, :about
+    get "/contact", StaticPagesController, :contact
   end
 
   # Other scopes may use custom stacks.
